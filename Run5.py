487248274ab5761d383d654c407aeb66479e6cde
ParadoxIsCoding <Tahasalman.9T@gmail.com>
2024-10-24 12:38:49 +1000

    updated code

--- Run5.py
+++ Run5.py
@@ -22,4 +22,15 @@ robot = DriveBase(left_motor,right_motor,62.4,104)
 robot.use_gyro(True)
 robot.settings(700,300,400,200)
  
-robot.straight(700)
+
+robot.straight(650)
+robot.turn(-53)
+robot.straight(300)
+LG.run_time(250,600,Stop.HOLD)
+robot.settings(1000,1000)
+robot.straight(-200)
+robot.settings(700,300)
+robot.turn(53)
+robot.straight(-500)
+
+
